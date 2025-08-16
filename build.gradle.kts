b618be01b75f43bdc027a2ff350fa0d4a4f69463
Fateme <fatemekhajavi2308@gmail.com>
2025-08-16 14:58:55 +0330

    final

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    // اضافه کردن پلاگین Google Services به صورت Kotlin DSL
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
